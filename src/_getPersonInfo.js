c6e00edfc8478ee9ebdfb58d2c9aa099ffe1d15b
yuxiaotu <yuxiaotu1024@qq.com>
2021-12-30 16:52:13 +0800

    javascript

--- src/_getPersonInfo.js
+++ src/_getPersonInfo.js
@@ -8,3 +8,13 @@ const person = 'Lydia',
 const age = 21;
 
 getPersonInfo`${person} is ${age} yeas old`
+
+/**
+ * ["", " is ", " years old"] 
+ * "Lydia"
+ * 21
+ */
+
+/**
+ * 第一个参数的值总是包含字符串的数组。其余的参数获取的是传递的表达式的值
+ */
